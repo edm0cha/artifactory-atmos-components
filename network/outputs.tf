aa5963666bc188e1db40242a9c0d31b9bd60808f
Edwin Moedano <edwin.moedano@caylent.com>
2025-07-09 12:05:50 -0600

    feat: added subnet outputs

--- network/outputs.tf
+++ network/outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
   value = aws_vpc.this.id
 }
+
+output "subnet_ids" {
+  value = [for s in aws_subnet.this : s.id]
+}
+
+output "subnet_cidrs" {
+  value = [for s in aws_subnet.this : s.cidr_block]
+}
